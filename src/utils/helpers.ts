be640c59fd86993c4f8186a1f59066ada8a9fbc5
Anton Borkovskij <a.borkovsky@yusmpgroup.com>
2025-09-08 03:34:09 +0300

    feat: currencies data & helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,7 @@
 import { RatesResponse } from '@/api/types/rates';
 
 import { RATES_LS_KEY } from './consts';
-import { RatesCache } from './types';
+import { Currency, RatesCache } from './types';
 
 export const saveRatesToLS = (payload: RatesResponse) => {
   const dataToLS = { data: payload, savedAt: Date.now() };
@@ -25,4 +25,46 @@ export const formatDate = (d?: Date | null) => {
     hour: '2-digit',
     minute: '2-digit',
   });
+};
+
+export const CURRENCIES = {
+  EUR: ['EUR', 'Euro', '€'],
+  USD: ['USD', 'United States Dollar', '$'],
+  JPY: ['JPY', 'Japanese Yen', '¥'],
+  BGN: ['BGN', 'Bulgarian Lev', 'лв'],
+  CZK: ['CZK', 'Czech Koruna', 'Kč'],
+  DKK: ['DKK', 'Danish Krone', 'kr'],
+  GBP: ['GBP', 'British Pound', '£'],
+  HUF: ['HUF', 'Hungarian Forint', 'Ft'],
+  PLN: ['PLN', 'Polish Złoty', 'zł'],
+  RON: ['RON', 'Romanian Leu', 'lei'],
+  SEK: ['SEK', 'Swedish Krona', 'kr'],
+  CHF: ['CHF', 'Swiss Franc', 'Fr'],
+  ISK: ['ISK', 'Icelandic Króna', 'kr'],
+  NOK: ['NOK', 'Norwegian Krone', 'kr'],
+  TRY: ['TRY', 'Turkish Lira', '₺'],
+  AUD: ['AUD', 'Australian Dollar', '$'],
+  BRL: ['BRL', 'Brazilian Real', 'R$'],
+  CAD: ['CAD', 'Canadian Dollar', '$'],
+  CNY: ['CNY', 'Chinese Yuan', '¥'],
+  HKD: ['HKD', 'Hong Kong Dollar', '$'],
+  IDR: ['IDR', 'Indonesian Rupiah', 'Rp'],
+  ILS: ['ILS', 'Israeli New Shekel', '₪'],
+  INR: ['INR', 'Indian Rupee', '₹'],
+  KRW: ['KRW', 'South Korean Won', '₩'],
+  MXN: ['MXN', 'Mexican Peso', '$'],
+  MYR: ['MYR', 'Malaysian Ringgit', 'RM'],
+  NZD: ['NZD', 'New Zealand Dollar', '$'],
+  PHP: ['PHP', 'Philippine Peso', '₱'],
+  SGD: ['SGD', 'Singapore Dollar', '$'],
+  THB: ['THB', 'Thai Baht', '฿'],
+  ZAR: ['ZAR', 'South African Rand', 'R'],
+};
+
+export const metaFor = (code: string): Currency => {
+  if (code in CURRENCIES) {
+    const upper = code.toUpperCase() as keyof typeof CURRENCIES;
+    return { code, name: CURRENCIES[upper][1], symbol: CURRENCIES[upper][2] };
+  }
+  return { code, name: code, symbol: '?' };
 };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
 import { RatesResponse } from '@/api/types/rates';
 
 export type RatesCache = { data: RatesResponse; savedAt: number };
+
+export type Currency = { code: string; name: string; symbol: string };
